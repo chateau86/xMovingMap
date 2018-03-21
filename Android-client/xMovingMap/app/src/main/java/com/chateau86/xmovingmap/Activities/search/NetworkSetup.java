95b7f9c878a387500b02f058973353ed815f66ae
Woradorn K <chateau86@gmail.com>
2018-03-20 21:34:43 -0400

    Added network connection UI.
    Signed-off-by: Woradorn K <chateau86@gmail.com>

---
+++ Android-client/xMovingMap/app/src/main/java/com/chateau86/xmovingmap/Activities/search/NetworkSetup.java
@@ -0,0 +1,13 @@
+package com.chateau86.xmovingmap.Activities.search;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NetworkSetup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_network_setup);
+    }
+}
